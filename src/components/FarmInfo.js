b546cb8e3d98bedd5573ca285c65cd25d83a7ac1
Marcelina <eileen.x6@gmail.com>
2017-12-12 17:05:01 +0100

    Added backgrounds and images

--- src/components/FarmInfo.js
+++ src/components/FarmInfo.js
@@ -4,7 +4,7 @@ import * as cnt from "./constants.js"
 export default class FarmInfo extends React.Component {
     render() {
         return (
-            <div>
+            <div className="farmInfo">
             <p>Farm amount: {this.props.farmAmount}</p>
             <p>Farm cost: {this.props.farmCost}</p>
             <p>Farm cookies production: {this.props.farmAmount * cnt.cpsMultiplier.farm}/second</p>
--- src/components/MineInfo.js
+++ src/components/MineInfo.js
@@ -4,7 +4,7 @@ import * as cnt from "./constants.js"
 export default class MineInfo extends React.Component {
     render() {
         return (
-            <div>
+            <div className="producerInfo">
             <p>Mine amount: {this.props.mineAmount}</p>
             <p>Mine cost: {this.props.mineCost}</p>
             <p>Mine cookies production: {this.props.mineAmount * cnt.cpsMultiplier.mine}/second</p>
