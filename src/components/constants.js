62724c87c645b77af82004d5d784748519ce2e42
Marcelina <eileen.x6@gmail.com>
2017-12-12 17:59:17 +0100

    Added cost multipliers

--- src/components/constants.js
+++ src/components/constants.js
@@ -6,5 +6,12 @@ const cpsMultiplier = {
         mine: 10,
         farm: 20
    }
+const costMultiplier = {
+    cursor: 15,
+    grandma: 50,
+    bakery: 100,
+    mine: 200,
+    farm: 500
+}   
 
-   export {cpsMultiplier}
+   export {cpsMultiplier, costMultiplier}
